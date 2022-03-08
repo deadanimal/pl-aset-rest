dfe3384b47a83c6e0c68fad28682506e67e400be
quantmis <realhost01@gmail.com>
2022-03-08 15:59:32 +0800

    update

--- resources/views/modul/stor/kewps1/index.blade.php
+++ resources/views/modul/stor/kewps1/index.blade.php
@@ -55,6 +55,7 @@
                                             {{ $ik1->no_kod }}<br>
                                         @endforeach
                                     </td>
+                                    {{$k1->status}}
 
                                     @if ($k1->status == 'DERAF')
                                         <td scope="col"><span class="badge bg-warning">{{ $k1->status }}</span></th>
@@ -106,6 +107,9 @@
                                                 </form>
                                             @endif
                                         </td>
+                                    @endif
+                                    @if ($k1->status == 'DERAF')
+                                    <a href="/kewps1" class="btn btn-primary btn-sm"><i class="fas fa-check-circle"></i></a>
                                     @endif
 
                                 </tr>
