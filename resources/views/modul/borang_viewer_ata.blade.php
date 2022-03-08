c669adab7f22740c776282a59b97ab0dca70f144
quantmis <realhost01@gmail.com>
2022-03-08 11:15:40 +0800

    fungsi cetakan plpkpa0102

--- resources/views/modul/borang_viewer_ata.blade.php
+++ resources/views/modul/borang_viewer_ata.blade.php
@@ -8,8 +8,8 @@
                         <h6 class="h2 text-white d-inline-block mb-0">Perbadanan Labuan</h6>
                         <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                             <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
-                                <li class="breadcrumb-item"><a href="#"><i class="fas fa-file"></i></a></li>
-                                <li class="breadcrumb-item"><a href="">{{ $title }}</a></li>
+                                <li class="breadcrumb-item"><a href="/{{$title}}"><i class="fas fa-file"></i></a></li>
+                                <li class="breadcrumb-item"><a href="/{{$title}}">{{ $title }}</a></li>
                             </ol>
                         </nav>
                     </div>
